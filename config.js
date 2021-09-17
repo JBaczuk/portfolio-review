c03c8d5e5c38f1bb0999fefd3368c18394ebf352
Vlad Solokha <48961183+vladsolokha@users.noreply.github.com>
2021-09-17 13:56:51 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'vlad solokha portfolio', // <title>
+  manifestName: 'Simple',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/vs-logo.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Vlad',
+  heading: 'Full Stack Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://linkedin.com/in/vlad-solokha',
+    },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/vladsolokha',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:vsolokha@gmail.com',
+    },
+  ],
+};
