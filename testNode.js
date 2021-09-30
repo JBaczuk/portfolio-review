ba587e9a617b5faf40827a3cb3b7707f595e3d32
Jordan Baczuk <jordan.baczuk@gmail.com>
2021-09-29 21:42:50 -0600

    add jordan suggestions

--- testNode.js
+++ testNode.js
@@ -1,34 +1,47 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const readline = require('readline')
+const readline = require("readline");
 
 const rd = readline.createInterface({
-  input: fs.createReadStream('./test.csv'),
+  input: fs.createReadStream("./test2.csv"),
   // output: process.stdout,
-  console: false
-})
+  console: false,
+});
 
-let data = {}
+let data = {};
 
-let lineCount = 0
-let headers = []
-rd.on('line', function (line) {
-  let arr = line.split(',')
+let lineCount = 0;
+let headers = [];
+rd.on("line", function (line) {
+  let arr = line.split(",");
   for (let i = 0; i < arr.length; i++) {
-    if (lineCount <= 1) headers.push(arr[i])
-    lineCount++
-    if (lineCount == 2) {
-      for (let i = 0; i < headers.length; i++) {
-        data[headers[i]] = []
-        lineCount++
-      }
+    if (lineCount < 1) {
+      data[arr[i]] = [];
     } else {
-      for (const key in data) {
-        console.log(i)
-        i == 0 ? data[key].push(arr[i]) : data[key].push(arr[i])
-        i++
-      }
+      data[Object.keys(data)[i]].push(arr[i]);
     }
   }
-  console.log('Data log', data)
-})
+  lineCount++;
+  // console.log("Data log", data);
+});
+
+// rd.on("line", function (line) {
+//   const lineItems = line.split(",");
+//   const headers = Object.keys(data);
+
+//   for (let i = 0; i < lineItems.length; i++) {
+//     const currentLineItem = lineItems[i];
+//     if (lineCount === 0) {
+//       data[currentLineItem] = [];
+//     } else {
+//       const currentHeader = headers[i];
+//       data[currentHeader].push(currentLineItem);
+//     }
+//   }
+
+//   lineCount++;
+// });
+
+rd.on("close", () => {
+  console.log("data", data);
+});
